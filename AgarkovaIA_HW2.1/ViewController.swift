1fbc776bac4386f33d7993059e9938f22427ee2b
irin <a_irina@utlook.com>
2019-09-18 14:35:17 +0300

    Initial Commit

---
+++ AgarkovaIA_HW2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AgarkovaIA_HW2.1
+//
+//  Created by Ирина Агаркова on 18/09/2019.
+//  Copyright © 2019 AgarkovaIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AgarkovaIA_HW2.1Tests/AgarkovaIA_HW2_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  AgarkovaIA_HW2_1Tests.swift
+//  AgarkovaIA_HW2.1Tests
+//
+//  Created by Ирина Агаркова on 18/09/2019.
+//  Copyright © 2019 AgarkovaIA. All rights reserved.
+//
+
+import XCTest
+@testable import AgarkovaIA_HW2_1
+
+class AgarkovaIA_HW2_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
